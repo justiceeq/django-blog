50e1145be976245b2f7769535f8aec400332a086
Eddie Quan <eddiequan@hotmail.com>
2014-09-07 11:16:39 -0400

    implemented listview for posts

--- eddieblog/urls.py
+++ eddieblog/urls.py
@@ -5,8 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'eddieblog.views.home', name='home'),
+    # url(r'^$', 'django_tutorial_blog_ng.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # Blog URLs
+    url(r'^.*$', include('blogengine.urls')),
 )
